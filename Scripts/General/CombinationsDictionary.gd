b05eabbd5a17eb82c5b3bb034d2086a79c582c5b
s27168 <s27168@pjwstk.edu.pl>
2025-08-03 10:37:52 +0200

    klaiwsze

--- Scripts/General/CombinationsDictionary.gd
+++ Scripts/General/CombinationsDictionary.gd
@@ -2,6 +2,10 @@ class_name CombinationsDictionary
 
 static var Combinations: Dictionary = {
 	"A": preload("res://Scenes/Notes/water_note.tscn"),
-	"S": preload("res://Scenes/Notes/cryo_note.tscn"),
-	"D": preload("res://Scenes/Notes/fire_note.tscn")
+	"S": preload("res://Scenes/Notes/fire_note.tscn"),
+	"D": preload("res://Scenes/Notes/earth_note.tscn"),
+	"F": preload("res://Scenes/Notes/wind_note.tscn"),
+	"J": preload("res://Scenes/Notes/cryo_note.tscn"),
+	"K": preload("res://Scenes/Notes/oil_note.tscn"),
+	"L": preload("res://Scenes/Notes/lightning_note.tscn")
 }
